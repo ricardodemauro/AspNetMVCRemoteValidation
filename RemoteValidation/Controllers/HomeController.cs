50b93e6d83e34735f5d20b8ead2904667891d2a4
Ricardo Mauro <ricardo.demauro@outlook.com>
2017-04-09 16:41:42 -0300

    initial commit.

---
+++ RemoteValidation/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using RemoteValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Remote Validation";
+            return View();
+        }
+
+        [HttpPost]
+        public string Index(SampleModel data)
+        {
+            return "saved";
+        }
+
+        public JsonResult CheckValidIP(string clientIp)
+        {
+            return Json(clientIp == "127.0.0.1", JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ RemoteValidation/Models/SampleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteValidation.Models
+{
+    public class SampleModel
+    {
+        [Required]
+        [Remote("CheckValidIP", "Home")]
+        public string ClientIp { get; set; }
+    }
+}
